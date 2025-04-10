baec1656064474ae9700481a770f5cbc4f972973
Mohammed Abdulqader Hashem Abdullah <m.abdulqader284@gmail.com>
2025-04-10 09:58:16 +0300

    Usecase4

--- websiteproject/public/js/learning-path.js
+++ websiteproject/public/js/learning-path.js
@@ -6,23 +6,63 @@ import instructorRepo from "./repository/Instructor.js";
 
 const cardsContainer = document.querySelector("#cards-container");
 
-document.addEventListener("DOMContentLoaded", loadMyLearningPath);
+const student = await studentRepo.getStudent(localStorage.username);
 
-async function loadMyLearningPath(e) {
-    const student = await studentRepo.getStudent("Mohammed");
-    const htmlArray = await Promise.all(
-        student.learningPath.map((course) => templateCourses(course))
-      );
-      cardsContainer.innerHTML = htmlArray.join("\n");
-    
+document.addEventListener("DOMContentLoaded", loadCourses());
+
+async function loadCourses(e) {
+  const courses = await studentRepo.getStudentCourses(student.username);
+
+  const htmlArray = await Promise.all(
+    courses.map((course) => templateCourses(course))
+  );
+  cardsContainer.innerHTML = htmlArray.join("\n");
 }
 
 async function templateCourses(course) {
+  const classes = await studentRepo.getStudentClasses(student.username);
+  const classes2 = student.classes;
+
+  const arr = [];
+  for (const clas of classes) {
+    if (clas.parentCourse != course.courseNo) {
+      continue;
+    } else {
+      arr.push(clas);
+    }
+  }
+  
+  const registeredClass = arr[0];
+  let grade =0;
+  for (const clas of classes2) {
+    if (clas.classNo != registeredClass.classNo) {
+      continue;
+    } else {
+      grade = clas.grade;
+    }
+  }
+
+
   return `
+    
     <div class="card">
-        <p>Name: ${course.name}</p>
-        <p>Category: ${course.category}</p>
-        <p>Number: ${course.courseNo}</p>
+        <div class="header">
+            <h2 class="title">${course.name}</h2>
+            <span class="category">Category: ${course.category}</span>
+        </div>
+        <div class="body">
+            <p class="desc">
+                ${course.description}
+            </p>
+            <p class="number">Course No: ${course.courseNo}</p>
+            <p>Status: ${course.status}</p>
+            
+        </div>
+        <div class="footer" style="color: black">
+            <p id="studentsNo">Number of students: ${registeredClass.noOfStudents}</p>
+            <p>Instructor: ${registeredClass.instructor} </p>
+            <p> Grade: ${grade} </p>
+        </div>
     </div>
     `;
 }
--- websiteproject/public/js/repository/Student.js
+++ websiteproject/public/js/repository/Student.js
@@ -1,6 +1,7 @@
 const baseUrl = "/api/students";
 import User from "./User.js";
-
+import courseRepo from "./Course.js";
+import classRepo from "./Class.js";
 class Student extends User {
   #id;
   #completedCourses;
@@ -29,37 +30,62 @@ class Student extends User {
 
   async createStudent(student) {
     return await fetch(baseUrl, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(student),
-      });
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(student),
+    });
   }
 
   async updateStudent(student) {
     return await fetch(baseUrl, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(student),
-      });
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(student),
+    });
   }
 
   async deleteStudent(student) {
     return await fetch(`${baseUrl}`, {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-        body: student,
-      });
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: student,
+    });
+  }
+
+  async getStudentClasses(username) {
+    const student = await this.getStudent(username);
+    if (student.error) return;
+    {
+      error: "Student not found";
+    }
+    const classes = await Promise.all(
+      student.classes.map((classItem) => classRepo.getClass(classItem.classNo))
+    );
+    return classes;
+  }
+
+  async getStudentCourses(username) {
+    const student = await this.getStudent(username);
+    if (student.error) return;
+    {
+      error: "Student not found";
+    }
+    const classes = await this.getStudentClasses(username);
+    const courses = await Promise.all(
+      classes.map((classItem) => courseRepo.getCourse(classItem.parentCourse))
+    );
+    return courses;
   }
 
   get id() {
     return this.#id;
   }
 
-  get completedCourses(){
+  get completedCourses() {
     return this.#completedCourses;
   }
 
-  get learningPath(){
+  get learningPath() {
     return this.#learningPath;
   }
 }
