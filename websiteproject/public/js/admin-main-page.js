eda7e735b229445b1be94752d3918382f8c8e78e
Mohammed Abdulqader Hashem Abdullah <m.abdulqader284@gmail.com>
2025-04-10 11:02:54 +0300

    .

--- websiteproject/public/js/admin-main-page.js
+++ websiteproject/public/js/admin-main-page.js
@@ -12,8 +12,6 @@ const student = await studentRepo.getStudent(localStorage.username);
 search.addEventListener("keyup", handleSearch);
 typeOfSearch.addEventListener("change", type);
 
-
-
 document.addEventListener("DOMContentLoaded", loadCourses());
 
 async function loadCourses(e) {
@@ -22,135 +20,10 @@ async function loadCourses(e) {
     courses.map((course) => templateCourses(course))
   );
   cardsContainer.innerHTML = htmlArray.join("\n");
-  
-  const selects = document.querySelectorAll(".classSelect");
-  
-  selects.forEach((select) => {
-    select.addEventListener("change", async (e) => {
-      const card = select.closest('.card');
-      
-      const studentsNo = card.querySelector("#studentsNo");
-      
-      if (e.target.value) {
-        const classNo = parseInt(e.target.value.split("-")[1]);
-
-        const classItem = await classRepo.getClass(classNo);
-
-        studentsNo.innerHTML = `Number of students: ${classItem.noOfStudents}`;
-      } else {
-
-        studentsNo.innerHTML = "Number of students: ";
-      }
-    });
-  });
-
-  const enroll = document.querySelectorAll(".enroll-btn");
-  enroll.forEach((btn) => {
-    btn.addEventListener("click", async (e) => {
-      const card = btn.closest(".card");
-      const select = card.querySelector(".classSelect");
-      const selectedValue = select.value;
-      const classNo = parseInt(selectedValue.split("-")[1]);
-      const clas = await classRepo.getClass(classNo);
-      const parentCourse = await courseRepo.getCourse(clas.parentCourse);
-      const errorMessage = card.querySelector(".error-message");
-      const approveMessage = card.querySelector(".approved-message");
-      // Did the student passed the course
-
-      if (clas.noOfStudents >= 30) {
-        errorMessage.innerHTML = "Class is full";
-        setTimeout(() => {
-          errorMessage.innerHTML = "";
-        }, 3000);
-        return;
-      }
-
-      if (parentCourse.status.toUpperCase() != "OPEN") {
-        errorMessage.innerHTML = "Course is not open for enrollment";
-        setTimeout(() => {
-          errorMessage.innerHTML = "";
-        }, 3000);
-        return;
-      }
-
-      for (let c of student.classes) {
-        if (
-          (await classRepo.getClass(c.classNo)).parentCourse ==
-            parentCourse.courseNo &&
-          (c.grade >= 60 )
-        ) {
-          errorMessage.innerHTML = "You already passed this course";
-          setTimeout(() => {
-            errorMessage.innerHTML = "";
-          }, 3000);
-          return;
-        }
-
-        if (
-          (await classRepo.getClass(c.classNo)).parentCourse ==
-            parentCourse.courseNo &&
-          (c.grade == null)
-        ) {
-          errorMessage.innerHTML = "You already registered in this course";
-          setTimeout(() => {
-            errorMessage.innerHTML = "";
-          }, 3000);
-          return;
-        }
-        
-
-        
-      }
-
-      if (
-        areListsEqual(
-          student.classes.map((cls) => cls.classNo),
-          parentCourse.prerequisites
-        ) == false
-      ) {
-        errorMessage.innerHTML = "You have to pass the prerequisites first";
-        setTimeout(() => {
-          errorMessage.innerHTML = "";
-        }, 3000);
-
-        return;
-      }
-
-      
-      student.classes.push({
-        classNo: classNo,
-        grade: null,
-      });
-      clas.noOfStudents++;
-      await studentRepo.updateStudent(student);
-      await classRepo.updateClass(clas);
-      approveMessage.innerHTML = "You have been enrolled successfully";
-      errorMessage.innerHTML = "";
-      setTimeout(() => {
-        approveMessage.innerHTML = "";
-      }, 3000);
-      setTimeout(() => {
-        errorMessage.innerHTML = "";
-      }, 3000);
-    });
-  });
-
 }
-
-function areListsEqual(list1, list2) {
-  if (list1.length !== list2.length) return false;
-
-  const sorted1 = [...list1].sort();
-  const sorted2 = [...list2].sort();
-
-  return sorted1.every((value, index) => value === sorted2[index]);
-}
-
-
 
 async function templateCourses(course) {
-  const classes = await Promise.all(course.classes.map((classItem) => classRepo.getClass(classItem)));
-  
+  // this is wrong, it need to be changed
   return `
     
     <div class="card">
@@ -164,16 +37,13 @@ async function templateCourses(course) {
             </p>
             <p class="number">Course No: ${course.courseNo}</p>
             <p>Status: ${course.status}</p>
-            <p id="studentsNo">Number of students: </p>
+               <p id="studentsNo">Number of students: </p>
+            <p>Instructor: </p>
+            <p> Grade:  </p>
         </div>
-        <div class="footer">
-            <select class="classSelect">
-                <option value="">Select instructor</option>
-                ${classes.map((classItem) => `<option value="${classItem.instructor}-${classItem.classNo}">${classItem.instructor}</option>`).join("")}
-            </select>
-            <button class="enroll-btn">Enroll</button>
-            <p class="error-message"> </p>
-            <p class="approved-message"> </p>
+        <div class="footer" style="color: black">
+         <button class="admin-btn admin-btn-approve">Approve</button>
+         <button class="admin-btn admin-btn-cancel">Cancel</button>
         </div>
     </div>
     `;
@@ -182,8 +52,6 @@ async function templateCourses(course) {
 async function type() {
   handleSearch();
 }
-
-
 
 async function handleSearch() {
   let selectedValue = typeOfSearch.value;
@@ -203,121 +71,5 @@ async function handleSearch() {
     courses.map((course) => templateCourses(course))
   );
   cardsContainer.innerHTML = htmlArray.join("\n");
-
-  //  عشان اذا سويت تحديث من البحث ما تخرب الخاصية
-
-  const selects = document.querySelectorAll(".classSelect");
-  selects.forEach((select) => {
-    select.addEventListener("change", async (e) => {
-      const card = select.closest('.card');
-      const studentsNo = card.querySelector("#studentsNo");
-      
-      if (e.target.value) {
-        const classNo = parseInt(e.target.value.split("-")[1]);
-        const classItem = await classRepo.getClass(classNo);
-        studentsNo.innerHTML = `Number of students: ${classItem.noOfStudents}`;
-      } else {
-        studentsNo.innerHTML = "Number of students: ";
-      }
-    });
-  });
-
-  const enroll = document.querySelectorAll(".enroll-btn");
-  enroll.forEach((btn) => {
-    btn.addEventListener("click", async (e) => {
-      const card = btn.closest(".card");
-      const select = card.querySelector(".classSelect");
-      const selectedValue = select.value;
-      const classNo = parseInt(selectedValue.split("-")[1]);
-      const clas = await classRepo.getClass(classNo);
-      const parentCourse = await courseRepo.getCourse(clas.parentCourse);
-      const errorMessage = card.querySelector(".error-message");
-      const approveMessage = card.querySelector(".approved-message");
-      // Did the student passed the course
-
-      if (clas.noOfStudents >= 30) {
-        errorMessage.innerHTML = "Class is full";
-        setTimeout(() => {
-          errorMessage.innerHTML = "";
-        }, 3000);
-        return;
-      }
-
-      if (parentCourse.status.toUpperCase() != "OPEN") {
-        errorMessage.innerHTML = "Course is not open for enrollment";
-        setTimeout(() => {
-          errorMessage.innerHTML = "";
-        }, 3000);
-        return;
-      }
-
-      for (let c of student.classes) {
-        if (
-          (await classRepo.getClass(c.classNo)).parentCourse ==
-            parentCourse.courseNo &&
-          (c.grade >= 60 )
-        ) {
-          errorMessage.innerHTML = "You already passed this course";
-          setTimeout(() => {
-            errorMessage.innerHTML = "";
-          }, 3000);
-          return;
-        }
-
-        if (
-          (await classRepo.getClass(c.classNo)).parentCourse ==
-            parentCourse.courseNo &&
-          (c.grade == null)
-        ) {
-          errorMessage.innerHTML = "You already registered in this course";
-          setTimeout(() => {
-            errorMessage.innerHTML = "";
-          }, 3000);
-          return;
-        }
-        
-
-        
-      }
-
-      if (
-        areListsEqual(
-          student.classes.map((cls) => cls.classNo),
-          parentCourse.prerequisites
-        ) == false
-      ) {
-        errorMessage.innerHTML = "You have to pass the prerequisites first";
-        setTimeout(() => {
-          errorMessage.innerHTML = "";
-        }, 3000);
-
-        return;
-      }
-
-      
-      student.classes.push({
-        classNo: classNo,
-        grade: null,
-      });
-      clas.noOfStudents++;
-      await studentRepo.updateStudent(student);
-      await classRepo.updateClass(clas);
-      approveMessage.innerHTML = "You have been enrolled successfully";
-      errorMessage.innerHTML = "";
-      setTimeout(() => {
-        approveMessage.innerHTML = "";
-      }, 3000);
-      setTimeout(() => {
-        errorMessage.innerHTML = "";
-      }, 3000);
-    });
-  });
 }
 
-/*
-
-    <div class="card">
-        <h1>Hello</h1>
-    </div>
-
-*/
